a4c9b2e60e01004524b5871fc88ca5dde093655e
KevinNobre <kevinchristian.nobre@gmail.com>
2025-06-01 12:44:24 -0300

    feat: OrbisDbContext

---
+++ Orbis.Infrastructure/Data/OrbisDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orbis.Infrastructure.Data
+{
+    public class OrbisDbContext : DbContext
+    {
+        public OrbisDbContext(DbContextOptions<OrbisDbContext> options) : base(options)
+        {
+        }
+    }
+}
