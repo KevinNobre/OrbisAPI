585a33e5829b0a41585f2db4ff39776782406ee2
KevinNobre <kevinchristian.nobre@gmail.com>
2025-06-02 12:39:04 -0300

    feat: Implementando Interface do Usuario

---
+++ Orbis.Domain/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using Orbis.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orbis.Domain.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        Task<IEnumerable<Usuario>> GetAllAsync();
+        Task<Usuario> GetByIdAsync(int id);
+        Task AddAsync(Usuario usuario);
+        Task UpdateAsync(Usuario usuario);
+        Task DeleteAsync(int id);
+
+    }
+}
