8337fdad894fcaf41c445fb4251129b5de2ee08f
KevinNobre <kevinchristian.nobre@gmail.com>
2025-06-02 14:57:32 -0300

    feat: Configurando documentação Swagger para UsuarioController

--- Orbis/Controllers/UsuarioController.cs
+++ Orbis/Controllers/UsuarioController.cs
@@ -15,6 +15,12 @@ namespace Orbis.Controllers
             _repository = repository;
         }
 
+        // Get all
+        /// <summary>
+        /// Obter todos os Usuarios
+        /// </summary>
+        /// <returns>Todos os Usuários </returns>
+        /// <response code="200">Sucesso</response>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Usuario>>> GetAll()
         {
@@ -22,6 +28,14 @@ namespace Orbis.Controllers
             return Ok(usuarios);
         }
 
+        /// <summary>
+        /// Obtém um usuário pelo ID.
+        /// </summary>
+        /// <param name="id">Identificador do Usuário</param>
+        /// <returns>Dados do Usuario</returns>
+        /// <response code="200">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="500">Erro interno</response>
         [HttpGet("{id}")]
         public async Task<ActionResult<Usuario>> GetById(int id)
         {
@@ -31,6 +45,17 @@ namespace Orbis.Controllers
             return Ok(usuario);
         }
 
+        // Create
+        /// <summary>
+        /// Cadastrar um Usuario 
+        /// </summary>
+        /// <remarks>
+        /// objeto Json
+        /// </remarks>
+        /// <param name="usuario">Dados do Usuario</param>
+        /// <returns>Usuário recém criado</returns>
+        /// <response code="201">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
         [HttpPost]
         public async Task<ActionResult<Usuario>> Create([FromBody] Usuario usuario)
         {
@@ -38,6 +63,16 @@ namespace Orbis.Controllers
             return CreatedAtAction(nameof(GetById), new { id = usuario.UsuarioId }, usuario);
         }
 
+
+        // Update
+        /// <summary>
+        /// Atualizar um Usuário
+        /// </summary>
+        /// <param name="id">Identificador do Usuario</param>
+        /// <param name="usuario">Dados do Usuário</param>
+        /// <returns>Não retorna informações</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(int id, [FromBody] Usuario usuario)
         {
@@ -58,6 +93,14 @@ namespace Orbis.Controllers
             return NoContent();
         }
 
+        // Delete
+        /// <summary>
+        /// Deletar um Usuario
+        /// </summary>
+        /// <param name="id">Identificador de Usuario</param>
+        /// <returns>Não retorna informações</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
