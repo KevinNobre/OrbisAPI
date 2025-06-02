95f670fce7d8817e099ff9035cc71e7b206159ca
KevinNobre <kevinchristian.nobre@gmail.com>
2025-06-02 13:45:48 -0300

    feat: Interface PedidoAjuda para Operações CRUD

---
+++ Orbis.Domain/Repositories/IPedidoAjudaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Orbis.Domain.Entities;
+
+namespace Orbis.Domain.Repositories
+{
+    public interface IPedidoAjudaRepository
+    {
+        Task<IEnumerable<PedidoAjuda>> GetAllAsync();
+        Task<PedidoAjuda> GetByIdAsync(int id);
+        Task AddAsync(PedidoAjuda pedido);
+        Task UpdateAsync(PedidoAjuda pedido);
+        Task DeleteAsync(int id);
+    }
+}
