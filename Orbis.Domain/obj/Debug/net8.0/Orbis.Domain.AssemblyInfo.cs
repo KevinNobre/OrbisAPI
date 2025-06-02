cd69884daf30509bbacf1fa1e2ce4a94a38a47e4
KevinNobre <kevinchristian.nobre@gmail.com>
2025-06-02 14:51:41 -0300

    fix: Entidade OngParceira para Crud e Tabela SQL

--- Orbis.Domain/obj/Debug/net8.0/Orbis.Domain.AssemblyInfo.cs
+++ Orbis.Domain/obj/Debug/net8.0/Orbis.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orbis.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2daeb17c2fa217e39f16cd20c0230c48ffcc59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+093126bbccb3a8a12069abe0c328d7b4f867b89c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orbis.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orbis.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
