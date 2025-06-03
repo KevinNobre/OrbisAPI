6a2c35dbec5cd1fc02db1adef5ec2f91688f6697
KevinNobre <kevinchristian.nobre@gmail.com>
2025-06-03 12:25:51 -0300

    feat: Implementando DTO

---
+++ Orbis.Application/DTO/PedidoAjudaDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orbis.Application.DTO
+{
+    public class PedidoAjudaDto
+    {
+        public int PedidoId { get; set; }
+        public string TipoAjuda { get; set; }
+        public string Urgencia { get; set; }
+        public string Descricao { get; set; }
+        public string Localidade { get; set; }
+        public string Status { get; set; }
+
+        public Dictionary<string, LinkDto> _links { get; set; }
+    }
+
+    public class LinkDto
+    {
+        public string Href { get; set; }
+        public string Method { get; set; }
+
+        public LinkDto(string href, string method)
+        {
+            Href = href;
+            Method = method;
+        }
+    }
+
+}
--- Orbis.Application/obj/Debug/net8.0/Orbis.Application.AssemblyInfo.cs
+++ Orbis.Application/obj/Debug/net8.0/Orbis.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orbis.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+093126bbccb3a8a12069abe0c328d7b4f867b89c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6834f8172b12cfdea58f913b24b5111ab02ba76f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orbis.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orbis.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
